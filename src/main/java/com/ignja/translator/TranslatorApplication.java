7894a984f503e906e2b16f9926b5fe302dc8d5f3
Milos Ignjatovic <milos.ignjatovic@boutsourcing.com>
2019-07-29 13:26:54 +0200

    UserResource (memory) /users route

--- src/main/java/com/ignja/translator/TranslatorApplication.java
+++ src/main/java/com/ignja/translator/TranslatorApplication.java
@@ -1,13 +1,17 @@
 package com.ignja.translator;
 
-import org.springframework.boot.SpringApplication;
+import javax.ws.rs.ApplicationPath;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TranslatorApplication {
+public class TranslatorApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TranslatorApplication.class, args);
+		new TranslatorApplication().configure(new SpringApplicationBuilder(TranslatorApplication.class)).run(args);
+		//SpringApplication.run(TranslatorApplication.class, args);
 	}
 
 }
